1eb4d1d645a6f0b91e59c2f601d8f0ae8fd9a8be
caphekoduong <61449974+caphekoduong@users.noreply.github.com>
2020-10-20 03:01:10 -0700

    Add files via upload

---
+++ Models/Attendance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATDSync.Models
+{
+    public class Attendance
+    {
+        public int id { get; set; }
+        public string MaNV { get; set; }
+        public DateTime tgQuet { get; set; }
+        public bool isSync { get; set; }
+    }
+}
